b978084a409aca5c0afe5a96e18f9bdf9b232bf7
LarissaCFarias <larissaconstantino@yahoo.com.br>
2019-03-09 23:18:59 -0300

    Second commit

---
+++ app/src/main/java/br/com/udemycouse/gtauth/CadPerfilActivity.java
@@ -0,0 +1,13 @@
+package br.com.udemycouse.gtauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_perfil);
+    }
+}
---
+++ app/src/main/java/br/com/udemycouse/gtauth/Login.java
@@ -0,0 +1,4 @@
+package br.com.udemycouse.gtauth;
+
+public class Login {
+}
